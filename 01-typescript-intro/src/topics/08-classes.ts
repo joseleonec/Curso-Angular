60b33ccf06e9af58728b96b12162128524a7d104
joseleonec <joleonca@gmail.com>
2024-08-05 10:37:55 -0500

    class constructor

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -3,10 +3,10 @@ export class Person {
     // public name: string;
     // private address: string;
 
-    constructor(public name: string, private address: string) {
-        this.name = name;
-        this.address = address;
-    }
+    constructor(
+        public name: string, 
+        private address: string = "NA"
+    ) { }
 
 
 }
