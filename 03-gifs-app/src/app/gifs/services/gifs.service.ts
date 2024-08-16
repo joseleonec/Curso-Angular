45a7643fa8cafc7d39a006ecc537d119edac7a8f
jose.leonc97 <jose.leonc97@ucuenca.edu.ec>
2024-08-16 11:27:15 -0500

    Validaciones servicio

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -8,15 +8,24 @@ export class GifsService {
     constructor() { }
 
     get tagsSearchHistory(): string[] {
+        // return the first 10 elements
         return [...this._tagsSearchHistory];
+    }
+
+    private orginizeTagsSearchHistory(tag: string) {
+        tag = tag.trim().toLowerCase();
+        if (this._tagsSearchHistory.includes(tag)) {
+            this._tagsSearchHistory = this._tagsSearchHistory.filter(t => t !== tag);
+        }
+        this._tagsSearchHistory.unshift(tag);
+        this._tagsSearchHistory = this._tagsSearchHistory.slice(0, 10);
     }
 
     searchTag(tag: string) {
         if (!tag) {
             return;
-          }
-        this._tagsSearchHistory.unshift(tag);
+        }
+        this.orginizeTagsSearchHistory(tag);
         console.log('Searching... ' + tag);
     }
-    
 }
